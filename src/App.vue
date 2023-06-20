5b850abb880bbb4d54cbe839ad9c00a850efc5a7
alep-2000 <alessiopiras056@gmail.com>
2023-06-20 17:38:53 +0200

    [S]Add style to header

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,7 @@ export default {
   </div>
 </template>
 
-<style lang="">
-  
+<style lang="scss">
+@use './styles/generals.scss' as *;
+@use './styles/./partials/variables' as *;
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,27 +7,66 @@ export default {
 <template lang="">
     <div>
         <header>
-            <div>
-                <img src="../../public/dc-logo.png" alt="Logo-bg">
-            </div>
-            <div>
-                <ul>
-                    <li><a href="#"> CHARACTERS </a></li>
-                    <li><a href="#"> COMICS </a></li>
-                    <li><a href="#"> MOVIES </a></li>
-                    <li><a href="#"> TV </a></li>
-                    <li><a href="#"> GAMES </a></li>
-                    <li><a href="#"> COLLECTIBLES </a></li>
-                    <li><a href="#"> VIDEOS </a></li>
-                    <li><a href="#"> FANS </a></li>
-                    <li><a href="#"> NEWS </a></li>
-                    <li><a href="#"> SHOP </a></li>
-                </ul>
+            <div class="flex">
+                <div>
+                    <img src="../../public/dc-logo.png" alt="Logo-bg">
+                </div>
+                <div>
+                    <ul>
+                        <li><a href="#"> CHARACTERS </a></li>
+                        <li><a href="#"> COMICS </a></li>
+                        <li><a href="#"> MOVIES </a></li>
+                        <li><a href="#"> TV </a></li>
+                        <li><a href="#"> GAMES </a></li>
+                        <li><a href="#"> COLLECTIBLES </a></li>
+                        <li><a href="#"> VIDEOS </a></li>
+                        <li><a href="#"> FANS </a></li>
+                        <li><a href="#"> NEWS </a></li>
+                        <li><a href="#"> SHOP </a></li>
+                    </ul>
+                </div>
             </div>
         </header>
     </div>
 </template>
 
-<style lang="">
-    
+<style lang="scss">
+@use '../styles/./partials/variables' as *;
+
+header {
+    width: 100%;
+    height: 100px;
+    background-color: red;
+
+    .flex {
+        display: flex;
+        justify-content: space-evenly;
+
+        img {
+            width: 60px;
+            margin: 20px 0;
+        }
+
+        ul {
+
+            li {
+                display: inline-block;
+                margin: 5px;
+                font-size: 10px;
+                margin: 40px 5px;
+
+                a {
+                    text-decoration: $text_decoration;
+                    color: black;
+
+                    &:hover {
+                        color: #0C7CEC;
+                        border-bottom: 3px solid #0C7CEC;
+                        padding-bottom: 46px;
+                    }
+                }
+            }
+        }
+    }
+}
 </style>
